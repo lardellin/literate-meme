8cc2bdcc2850c736251b8eec295b68e10c3624da
LARD2 <nicolo.lardelli@coop.ch>
2024-01-26 09:01:56 +0100

    porting to new repo what I did yesterday

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+struct PokemonProduct {
+    url: Option<String>,
+    image: Option<String>,
+    name: Option<String>,
+    price: Option<String>
+}
+
+fn main() {
+    // download the target HTML document
+    let response = reqwest::blocking::get("https://scrapeme.live/shop/");
+    // get the HTML content from the request response
+    // and print it
+    let html_content = response.unwrap().text().unwrap();
+
+    // generate stuff
+    let document = scraper::Html::parse_document(&html_content);
+
+    let html_product_selector = scraper::Selector::parse("li.product").unwrap();
+
+    let html_products = document.select(&html_product_selector);
+
+    let mut pokemon_products: Vec<PokemonProduct> = Vec::new();
+
+    for html_product in html_products {
+        // retrieve the info from the fields of interest
+        // println!("{html_product}")
+        let url = html_product
+            .select(&scraper::Selector::parse("a").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("href"))
+            .map(str::to_owned);
+
+        let image = html_product
+            .select(&scraper::Selector::parse("img").unwrap())
+            .next()
+            .and_then(|img| img.value().attr("src"))
+            .map(str::to_owned);
+
+        let name = html_product
+            .select(&scraper::Selector::parse("h2").unwrap())
+            .next()
+            .map(|h2| h2.text().collect::<String>());
+
+        let price = html_product
+            .select(&scraper::Selector::parse(".price").unwrap())
+            .next()
+            .map(|price| price.text().collect::<String>());
+
+        let name2print = name.clone().unwrap();
+        println!("{name2print}");
+        let pokemon_product = PokemonProduct {
+            url,
+            image,
+            name,
+            price,
+        };
+        pokemon_products.push(pokemon_product);
+        
+    }
+}
